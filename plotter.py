1340e4f70408ef319ae119f7c51c3f12dfe28a87
sarahzeghendy <sarazghendy@gmail.com>
2025-05-16 10:55:57 +0200

    UPDATED V4

--- plotter.py
+++ plotter.py
@@ -221,4 +221,4 @@ def update_output(contents, selected_metric):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
